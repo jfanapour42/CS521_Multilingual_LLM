5b32fc9c939c2f3330d38656eed4b5a741081e53
Jordan Fanapour <jjfanapour@gmail.com>
2024-04-24 19:22:17 -0500

    Fix preprocess english script.

--- scripts/preprocess/en.sh
+++ scripts/preprocess/en.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-SPLIT=00  # Presplitted for parallel processing
+SPLIT=00                                          # Presplitted for parallel processing
 
-DOWNLOAD_DATA_DIR="SET_ME"
-OUTPUT_DIR="SET_ME"
-mkdir -p $OUTPUT_DIR/English00/
-mkdir -p $OUTPUT_DIR/English00_pretraining/
+DOWNLOAD_DATA_DIR="../Data/raw_download"       # Set to your directory path
+OUTPUT_DIR="../Data/processed"                 # Set to your directory path
+mkdir -p $OUTPUT_DIR/English/
+mkdir -p $OUTPUT_DIR/English_pretraining/
 
 # Convert data
-python3 src/utils/get_wiki_text.py --input_directory $DOWNLOAD_DATA_DIR/English00/ --output_directory $OUTPUT_DIR/English00/
+python src/utils/get_wiki_text.py --input_directory $DOWNLOAD_DATA_DIR/English/ --output_directory $OUTPUT_DIR/English/
 
 # Preprocess
-python3 src/utils/preprocess.py \
-  --source_directory $OUTPUT_DIR/English00/ \
-  --output_directory $OUTPUT_DIR/English00_pretraining/ \
+python src/utils/preprocess.py \
+  --source_directory $OUTPUT_DIR/English/ \
+  --output_directory $OUTPUT_DIR/English_pretraining/ \
   --tokenizer_path xlm-roberta-base \
   --max_seq_len 512 \
   --min_load_len 10 \
